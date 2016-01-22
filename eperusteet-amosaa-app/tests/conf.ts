944c00b1e235fac4c6d5d07c078a63fd3cee7b89
Niko Junkala <niko.junkala@gofore.com>
2016-01-22 17:58:26 +0200

    Testattu lisää komponentteja

--- eperusteet-amosaa-app/tests/conf.ts
+++ eperusteet-amosaa-app/tests/conf.ts
@@ -1,4 +1,7 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['e2e/sandbox.js']
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['e2e/result.js'],
+    jasmineNodeOpts: {
+        showColors: true
+    }
 };
--- eperusteet-amosaa-app/tests/e2e/sandbox.ts
+++ eperusteet-amosaa-app/tests/e2e/sandbox.ts
@@ -1,12 +1,41 @@
-describe('Sandbox components should work', () => {
-    browser.get('http://localhost:9030/#/fi/sandbox');
+describe("Sandbox components", () => {
+    browser.get("http://localhost:9030/#/fi/sandbox");
 
-    it('should translate objects', () => {
-        expect($('div[kaanna=obj]').getText()).toEqual('Hei');
+    it("should translate objects", () => {
+        expect($("div[kaanna=obj]").getText()).toEqual("Hei");
     });
 
-    it('Editointikontrollit pitäisi toimia globaalilla valitsimella', () => {
-        const muokkaa = element.all(by.buttonText('Muokkaa')).get(1);
-        expect(muokkaa).not.toBeFalsy();
+    it("Spinneri näkyy", () => {
+        expect($("div[us-spinner]").isDisplayed()).toBeFalsy();
+        const spinner = element(by.buttonText("Toggle spinner"));
+
+        spinner.click();
+        expect($("div[us-spinner]").isDisplayed()).toBeTruthy();
+
+        spinner.click();
+        expect($("div[us-spinner]").isDisplayed()).toBeFalsy();
     });
+
+    it("Editointikontrollit pitäisi toimia globaalilla valitsimella", () => {
+        const muokkaa = element.all(by.buttonText("Muokkaa")).get(1);
+        expect(muokkaa.isEnabled()).not.toBeFalsy();
+        muokkaa.click();
+        Helper.allDisabled(by.buttonText("Muokkaa"));
+
+        const tallenna = element(by.buttonText("tallenna"));
+        expect(tallenna.isEnabled()).not.toBeFalsy();
+        tallenna.click();
+        Helper.allEnabled(by.buttonText("Muokkaa"));
+    });
+
+    it("Editointikontrollit pitäisi toimia lokaaleilla valitsimella", () => {
+        const muokkaa = element.all(by.buttonText("Muokkaa")).get(0);
+        muokkaa.click();
+        Helper.allDisabled(by.buttonText("Muokkaa"));
+
+        const cancel = element(by.buttonText("Cancel"));
+        cancel.click();
+        Helper.allEnabled(by.buttonText("Muokkaa"));
+    });
+
 });
