98e0d22b2b8b960b020a0bbb1b3162cfec31a9b6
Niko Junkala <niko.junkala@gofore.com>
2016-02-05 09:18:45 +0200

    Poistettu käyttäjätietojen hakeminen

--- eperusteet-amosaa-app/src/app/states/state.ts
+++ eperusteet-amosaa-app/src/app/states/state.ts
@@ -3,17 +3,19 @@ angular.module("app")
 .state("root", {
     url: "/:lang",
     resolve: {
-        kayttaja: Api => Api.one("kayttaja"),
-        kayttajaKoulutustoimijat: kayttaja => kayttaja.one("koulutustoimijat").get(),
+        // kayttaja: Api => Api.one("kayttaja"),
+        // kayttajaKoulutustoimijat: kayttaja => kayttaja.one("koulutustoimijat").get(),
         // casMe: () => Kayttaja.casMe(),
         // casRoles: () => Kayttaja.casRoles(),
         kayttajaprofiili: () => Fake.Kayttajaprofiili(1)
     },
     views: {
         "": {
-            controller: ($scope, $state, kayttajaKoulutustoimijat) => {
+            // controller: ($scope, $state, kayttajaKoulutustoimijat) => {
+            controller: ($scope, $state) => {
                 // FIXME Find generic solution
-                const redirect = () => $state.go("root.koulutustoimija.detail", { ktId: kayttajaKoulutustoimijat[0].koulutustoimija });
+                // const redirect = () => $state.go("root.koulutustoimija.detail", { ktId: kayttajaKoulutustoimijat[0].koulutustoimija });
+                const redirect = () => $state.go("root.koulutustoimija.detail", { ktId: 1 });
                 $scope.$on("$stateChangeSuccess", (_, state) => {
                     if (state.name === "root") {
                         redirect();
