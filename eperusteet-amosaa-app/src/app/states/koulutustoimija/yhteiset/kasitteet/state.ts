f2468fe10272dda7c499bea7c2f1f6e23d489f56
Niko Junkala <niko.junkala@gofore.com>
2016-03-10 18:14:07 +0200

    Lisätty createListRestangular listojen editointia varten

--- eperusteet-amosaa-app/src/app/states/koulutustoimija/yhteiset/kasitteet/state.ts
+++ eperusteet-amosaa-app/src/app/states/koulutustoimija/yhteiset/kasitteet/state.ts
@@ -4,45 +4,15 @@ angular.module("app")
     url: "/kasitteet",
     resolve: {
         kasitteet: (koulutustoimija) => koulutustoimija.all('termisto').getList(),
-        //experimenting
-        //lista: (kasitteet) => Termisto.rakenna(kasitteet)
     },
     views: {
         "": {
             controller: ($scope, $rootScope, kasitteet) => {
-
-                $scope.kasitteet = kasitteet;
-
-                const createKasitteet = () => {
-                    $scope.kasitteet = _(kasitteet).map((kasite, idx) => {
-                            kasite.$$edit = EditointikontrollitService.createRestangular($scope.kasitteet, idx, kasite);
-                            return kasite;
-                        }).sortBy('termi').value();
-                };
-
-                createKasitteet();
-
-                $scope.selected = {value: undefined};
-
-                $rootScope.$on('editointikontrollit:cancel', (e) => {
-                    $scope.selected = {value: undefined}
-                });
-
-                const refresh = (kasite) => {
-                    $scope.kasitteet = _.without($scope.kasitteet, kasite);
-                }
-
-                $scope.delete = (kasite) => {
-                    let options = { name: kasite.termi };
-                    ModalConfirm.generalConfirm(options, kasite).then((kasite) => {
-                        if (kasite) {
-                            kasite.remove().then(() => {
-                                NotifikaatioService.onnistui("poistaminen-onnistui");
-                                refresh(kasite);
-                            });
-                        }
-                    });
-                };
+                $scope.edit = EditointikontrollitService.createListRestangular($scope, "kasitteet", kasitteet);
+                $scope.remove = (kasite) =>
+                    ModalConfirm.generalConfirm({ name: kasite.termi }, kasite)
+                        .then(kasite => kasite.remove())
+                        .then(() => _.remove($scope.kasitteet, kasite));
 
             }
 
--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/ops/impl/TermistoServiceImpl.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/ops/impl/TermistoServiceImpl.java
@@ -16,14 +16,14 @@
 
 package fi.vm.sade.eperusteet.amosaa.service.ops.impl;
 
-import fi.vm.sade.eperusteet.amosaa.service.ops.TermistoService;
-import fi.vm.sade.eperusteet.amosaa.domain.koulutustoimija.Koulutustoimija;
 import fi.vm.sade.eperusteet.amosaa.domain.Termi;
+import fi.vm.sade.eperusteet.amosaa.domain.koulutustoimija.Koulutustoimija;
 import fi.vm.sade.eperusteet.amosaa.dto.ops.TermiDto;
 import fi.vm.sade.eperusteet.amosaa.repository.koulutustoimija.KoulutustoimijaRepository;
 import fi.vm.sade.eperusteet.amosaa.repository.koulutustoimija.TermistoRepository;
 import fi.vm.sade.eperusteet.amosaa.service.exception.NotExistsException;
 import fi.vm.sade.eperusteet.amosaa.service.mapping.DtoMapper;
+import fi.vm.sade.eperusteet.amosaa.service.ops.TermistoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -41,62 +41,62 @@ public class TermistoServiceImpl implements TermistoService {
     private DtoMapper mapper;
 
     @Autowired
-    TermistoRepository termisto;
+    TermistoRepository termistoRepository;
 
     @Autowired
-    KoulutustoimijaRepository koulutustoimija;
+    KoulutustoimijaRepository koulutustoimijaRepository;
 
     @Override
     @Transactional(readOnly = true)
     public List<TermiDto> getTermit(Long baseId) {
-        Koulutustoimija toimija = koulutustoimija.findOne(baseId);
-        List<Termi> termit = termisto.findAllByKoulutustoimija(toimija);
+        Koulutustoimija toimija = koulutustoimijaRepository.findOne(baseId);
+        List<Termi> termit = termistoRepository.findAllByKoulutustoimija(toimija);
         return mapper.mapAsList(termit, TermiDto.class);
     }
 
     @Override
     @Transactional(readOnly = true)
     public TermiDto getTermi(Long baseId, Long id) {
-        Koulutustoimija toimija = koulutustoimija.findOne(id);
-        Termi termi = termisto.findOneByKoulutustoimijaAndId(toimija, id);
+        Koulutustoimija toimija = koulutustoimijaRepository.findOne(baseId);
+        Termi termi = termistoRepository.findOneByKoulutustoimijaAndId(toimija, id);
         return mapper.map(termi, TermiDto.class);
     }
 
     @Override
     @Transactional(readOnly = true)
     public TermiDto getTermiByAvain(Long baseId, String avain) {
-        Koulutustoimija toimija = koulutustoimija.findOne(baseId);
-        Termi termi = termisto.findOneByKoulutustoimijaAndAvain(toimija, avain);
+        Koulutustoimija toimija = koulutustoimijaRepository.findOne(baseId);
+        Termi termi = termistoRepository.findOneByKoulutustoimijaAndAvain(toimija, avain);
         return mapper.map(termi, TermiDto.class);
     }
 
     @Override
     public TermiDto addTermi(Long baseId, TermiDto dto) {
-        Koulutustoimija toimija = koulutustoimija.findOne(baseId);
+        Koulutustoimija toimija = koulutustoimijaRepository.findOne(baseId);
         assertExists(toimija, "Koulutustoimija ei ole olemassa");
         Termi tmp = mapper.map(dto, Termi.class);
         tmp.setKoulutustoimija(toimija);
-        tmp = termisto.save(tmp);
+        tmp = termistoRepository.save(tmp);
         return mapper.map(tmp, TermiDto.class);
     }
 
     @Override
     public TermiDto updateTermi(Long baseId, TermiDto dto) {
-        Koulutustoimija toimija = koulutustoimija.findOne(baseId);
+        Koulutustoimija toimija = koulutustoimijaRepository.findOne(baseId);
         assertExists(toimija, "Koulutustoimija ei ole olemassa");
-        Termi current = termisto.findOne(dto.getId());
+        Termi current = termistoRepository.findOne(dto.getId());
         assertExists(current, "Päivitettävää tietoa ei ole olemassa");
         mapper.map(dto, current);
-        termisto.save(current);
+        termistoRepository.save(current);
         return mapper.map(current, TermiDto.class);
     }
 
     @Override
     public void deleteTermi(Long baseId, Long id) {
-        Koulutustoimija toimija = koulutustoimija.findOne(baseId);
+        Koulutustoimija toimija = koulutustoimijaRepository.findOne(baseId);
         assertExists(toimija, "Koulutustoimija ei ole olemassa");
-        Termi termi = termisto.findOneByKoulutustoimijaAndId(toimija, id);
-        termisto.delete(termi);
+        Termi termi = termistoRepository.findOneByKoulutustoimijaAndId(toimija, id);
+        termistoRepository.delete(termi);
     }
 
     private static void assertExists(Object o, String msg) {
