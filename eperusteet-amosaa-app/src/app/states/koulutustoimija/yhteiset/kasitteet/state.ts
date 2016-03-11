22379706f69cd2ff28265588f5bcf6930e034634
Richard Van Camp <richard.vancamp@gofore.com>
2016-03-11 17:45:08 +0200

    new kasite placeholder row

--- eperusteet-amosaa-app/src/app/states/koulutustoimija/yhteiset/kasitteet/state.ts
+++ eperusteet-amosaa-app/src/app/states/koulutustoimija/yhteiset/kasitteet/state.ts
@@ -7,7 +7,22 @@ angular.module("app")
     },
     views: {
         "": {
-            controller: ($scope, $rootScope, kasitteet) => {
+            controller: ($scope, $rootScope, kasitteet, $log) => {
+                $scope.newKasite = {
+                    placeholder: {termi: "", selitys: "", alaviite: null},
+                    creating: false
+                };
+                $scope.createKasite = () => {
+                    $log.info("creating");
+                    $scope.newKasite.creating = true;
+                };
+                $scope.postKasite = (newKasite) => {
+                    //validate here
+                    $scope.newKasite.creating = false;
+                    kasitteet.post(newKasite).then((res) => {
+                        $scope.kasittet.unshift(res);
+                    })
+                };
                 $scope.edit = EditointikontrollitService.createListRestangular($scope, "kasitteet", kasitteet);
                 $scope.remove = (kasite) =>
                     ModalConfirm.generalConfirm({ name: kasite.termi }, kasite)
