7c4e86fe653ba074ebe99bf36176ee141194ae8b
Richard Van Camp <richard.vancamp@gofore.com>
2016-03-13 21:50:35 +0200

    sorting for termisto table

--- eperusteet-amosaa-app/src/app/logic/termisto.ts
+++ eperusteet-amosaa-app/src/app/logic/termisto.ts
@@ -1,12 +1,36 @@
 namespace Termisto {
-    export const sortAZ = (kasitteet: Array<any>) => {
-        return kasitteet;
+    const termistoViestit= {
+        serverError: ()=> KaannaService.kaanna("palvelin-virhe"),
+        validationError: ()=> KaannaService.kaanna("termi-puuttuu"),
+        postSuccess: ()=> KaannaService.kaanna("kasite-tallenettu")
     };
-    export const rakenna = (kasitteet: Array<any>) => {
-        return kasitteet;
+    const makeKey = (termi) => {
+        let key = KaannaService.kaanna(termi).replace(/[^a-zA-Z0-9]/g, '') || 'avain';
+        return key + (new Date()).getTime();
     };
-    export const makeKey = (item) => {
-        let termi = _.first(_.compact(_.values(item.termi))) || 'avain';
-        return termi.replace(/[^a-zA-Z0-9]/g, '') + (new Date()).getTime();
+    const validate = (termi) => {
+        return !!KaannaService.kaanna(termi).trim();
+    };
+    const readyToPost = (newKasite) => {
+        console.log("here");
+        if (!validate(newKasite.termi)) {
+            NotifikaatioService.varoitus(termistoViestit.serverError);
+            return false;
+        }
+        if (!newKasite.avain) {
+            newKasite.avain =  makeKey(newKasite.termi);
+        }
+        return newKasite;
+    };
+    const handleResponse = (kasite) => {
+        NotifikaatioService.onnistui("Uusi käsite on tallenettu");
+        return kasite;
+    };
+    const handleError = (err) => {
+        NotifikaatioService.varoitus(termistoViestit.validationError, err);
+    };
+    export const post = (kasitteet, newKasite) => {
+        let post = readyToPost(newKasite);
+        return post ? kasitteet.post(post).then(handleResponse).catch(handleError) : false;
     }
 };
--- eperusteet-amosaa-app/src/app/states/koulutustoimija/yhteiset/kasitteet/state.ts
+++ eperusteet-amosaa-app/src/app/states/koulutustoimija/yhteiset/kasitteet/state.ts
@@ -7,25 +7,28 @@ angular.module("app")
     },
     views: {
         "": {
-            controller: ($scope, $rootScope, kasitteet, $log, Termisto) => {
-                //$scope.newKasite = {};
+            controller: ($scope, $rootScope, kasitteet, $log) => {
+                $scope.newKasite = {};
                 $scope.creatingNewKasite = false;
                 $scope.cancel = () => $scope.creatingNewKasite = false;
                 $scope.createKasite = () => $scope.creatingNewKasite = true;
                 $scope.postKasite = (newKasite) => {
-                    if (!newKasite.avain) {
-                        newKasite.avain =  Termisto.makeKey(newKasite);
-                    }
                     $scope.creatingNewKasite = false;
-                    kasitteet.post(newKasite).then((kasite) => {
-                        $scope.kasitteet.unshift(kasite);
-                    })
+                    Termisto.post(kasitteet, newKasite)
+                        .then((res) => {
+                            $scope.kasitteet.unshift(res);
+                            $scope.newKasite = {};
+                        })
                 };
                 $scope.edit = EditointikontrollitService.createListRestangular($scope, "kasitteet", kasitteet);
                 $scope.remove = (kasite) =>
                     ModalConfirm.generalConfirm({ name: kasite.termi }, kasite)
                         .then(kasite => kasite.remove())
                         .then(() => _.remove($scope.kasitteet, kasite));
+
+                $scope.sortByAlaviite = (order) => {
+                    $scope.kasitteet = _.sortBy($scope.kasitteet, (k) => k.alaviite === order);
+                }
 
             }
 
