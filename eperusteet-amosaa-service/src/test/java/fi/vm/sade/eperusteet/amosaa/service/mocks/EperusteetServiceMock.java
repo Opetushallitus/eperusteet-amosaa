1d29a61454b458c0fbc1b8482955eaf4c9f2a90d
Niko Junkala <niko.junkala@gofore.com>
2016-04-07 18:39:47 +0300

    Korjattu rikkinäinen testi

--- eperusteet-amosaa-service/src/test/java/fi/vm/sade/eperusteet/amosaa/service/mocks/EperusteetServiceMock.java
+++ eperusteet-amosaa-service/src/test/java/fi/vm/sade/eperusteet/amosaa/service/mocks/EperusteetServiceMock.java
@@ -55,7 +55,7 @@ public class EperusteetServiceMock implements EperusteetService {
     }
 
     @Override
-    public String getPerusteString(String diaarinumero) {
+    public String getPerusteData(Long id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
