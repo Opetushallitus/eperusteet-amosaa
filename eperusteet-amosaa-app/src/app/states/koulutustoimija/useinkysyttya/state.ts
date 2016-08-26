7d4dbb7381531f9684ee81a58db9af06f334f21d
Niko Junkala <niko.junkala@gofore.com>
2016-08-26 12:31:48 +0300

    Korjauksia useiten kysyttyihin

--- eperusteet-amosaa-app/src/app/states/koulutustoimija/useinkysyttya/state.ts
+++ eperusteet-amosaa-app/src/app/states/koulutustoimija/useinkysyttya/state.ts
@@ -12,61 +12,61 @@ angular.module("app")
                     && _.isString(search)
                     && target.toLowerCase().indexOf(search.toLowerCase()) !== -1;
 
-                _.merge($scope, {
-                    koulutustoimija,
-                    faq: faq.reverse(),
-                    canEdit: Koulutustoimijat.isOpetushallitus(koulutustoimija),
-                    search: "",
-                    suodata: (search) => {
-                        _.each($scope.faq, item => {
-                            item.$$filtered = !search || _.isEmpty(search) || contains(item.kysymys, search) || contains(item.vastaus, search);
-                        });
-                    },
-                    editing: {
-                        add: () => {
-                            $scope.faq.unshift({
-                                kysymys: "",
-                                vastaus: "",
-                                $$filtered: true
-                            });
-                            $timeout(() => {
-                                $scope.editing.start($scope.faq[0]);
-                            })
-                        },
-                        start: (item) => {
-                            $scope.$$isEditing = true;
-                            item.$$kysymys = item.kysymys;
-                            item.$$vastaus = item.vastaus;
-                            item.$$editing = true;
-                        },
-                        save: (item) => {
-                            $rootScope.$broadcast("notifyCKEditor");
-                            if (item.id) {
-                                faq.customPUT(item, item.id);
-                            }
-                            else {
-                                faq.post(item)
-                                    .then((res) => _.merge(item, res));
-                            }
-                            item.$$editing = false;
-                            $scope.$$isEditing = false;
-                        },
-                        cancel: (item) => {
-                            item.$$editing = false;
-                            item.kysymys = item.$$kysymys;
-                            item.vastaus = item.$$vastaus;
-                            if (!item.id) {
-                                _.remove($scope.faq, item);
-                            }
-                            $scope.$$isEditing = false;
-                        },
-                        remove: (item) => {
-                            item.remove();
-                            _.remove($scope.faq, item);
-                            $scope.$$isEditing = false;
-                        }
+                $scope.koulutustoimija;
+                $scope.faq = faq.reverse();
+                $scope.canEdit = Koulutustoimijat.isOpetushallitus(koulutustoimija);
+                $scope.search = "";
+
+                $scope.suodata = (search) => {
+                    _.each($scope.faq, item => {
+                        item.$$filtered = !search || _.isEmpty(search) || contains(item.kysymys, search) || contains(item.vastaus, search);
+                    });
+                };
+
+                $scope.add = () => {
+                    $scope.faq.unshift({
+                        kysymys: "",
+                        vastaus: "",
+                        $$filtered: true
+                    });
+                    $scope.start($scope.faq[0]);
+                };
+
+                $scope.start = (item) => {
+                    $scope.$$isEditing = true;
+                    item.$$kysymys = item.kysymys;
+                    item.$$vastaus = item.vastaus;
+                    item.$$editing = true;
+                };
+
+                $scope.save = (item) => {
+                    $rootScope.$broadcast("notifyCKEditor");
+                    if (item.id) {
+                        faq.customPUT(item, item.id);
                     }
-                });
+                    else {
+                        faq.post(item)
+                            .then((res) => _.merge(item, res));
+                    }
+                    item.$$editing = false;
+                    $scope.$$isEditing = false;
+                };
+
+                $scope.cancel = (item) => {
+                    item.$$editing = false;
+                    item.kysymys = item.$$kysymys;
+                    item.vastaus = item.$$vastaus;
+                    if (!item.id) {
+                        _.remove($scope.faq, item);
+                    }
+                    $scope.$$isEditing = false;
+                };
+
+                $scope.remove = (item) => {
+                    item.remove();
+                    _.remove($scope.faq, item);
+                    $scope.$$isEditing = false;
+                };
 
                 $scope.suodata();
             }
