bfa50331bf27a3a36d4a92438545f70e57736516
Niko Junkala <niko.junkala@gofore.com>
2016-09-27 16:00:18 +0300

    Korjattu testit

--- eperusteet-amosaa-app/tests/e2e/koulutustoimijan-nakyma.ts
+++ eperusteet-amosaa-app/tests/e2e/koulutustoimijan-nakyma.ts
@@ -4,46 +4,31 @@ describe("Koulutustoimijan näkymä", () => {
 
         browser.get("http://amosaa:amosaa@localhost:9030/");
 
-        element(by.css("h2 > span > a[uib-dropdown-toggle]")).click();
-
+        element(by.css("h2 > div > span > a[uib-dropdown-toggle]")).click();
         element(by.linkText("Opetushallitus")).click();
-
         element.all(by.buttonText("Lisää uusi")).get(1).click();
-
         element(by.css("input[ng-model=\"pohja.nimi\"]")).sendKeys("pohja");
-
         element(by.buttonText("Lisää")).click();
 
         element(by.css("a[ng-click=\"muutaTila()\"]")).click();
         element(by.css("div[ng-if=\"tila === 'valmis'\"]")).click();
         element(by.css("a[ng-click=\"muutaTila()\"]")).click();
         element(by.css("div[ng-if=\"tila === 'julkaistu'\"]")).click();
-
         element(by.css("a[ui-sref=\"root\"]")).click();
 
     });
 
     it("Uuden yhteisen osan luominen", () => {
-
         element.all(by.buttonText("Lisää uusi")).get(0).click();
-
         element(by.css("input[ng-model=\"yhteinen.nimi\"]")).sendKeys("pohjan testi");
-
         element(by.buttonText("Lisää")).click();
-
         element(by.css("a[ui-sref=\"root\"]")).click();
-
     });
 
     it("Jaetun tutkinnon luominen", () => {
-
         element.all(by.buttonText("Lisää jaettu osa")).get(1).click();
-
         element(by.css("input[ng-model=\"ops.nimi\"]")).sendKeys("jaettu");
-
         element(by.buttonText("Tallenna")).click();
-
         element(by.css("a[ui-sref=\"root\"]")).click();
-
     });
 });
--- eperusteet-amosaa-app/tests/e2e/opetussuunnitelman-nakyma.ts
+++ eperusteet-amosaa-app/tests/e2e/opetussuunnitelman-nakyma.ts
@@ -4,7 +4,7 @@ describe("Koulutustoimijan näkymä", () => {
         browser.get("http://amosaa:amosaa@localhost:9030/");
 
         expect($("button[ng-click='addOpetussuunnitelma()']").isPresent());
-        const luoOps = element.all(by.buttonText("Lisää opetussuunnitelma")).get(1);
+        const luoOps = element.all(by.buttonText("Opetussuunnitelma")).get(1);
         expect(luoOps.isEnabled()).not.toBeFalsy();
         luoOps.click();
 
@@ -28,81 +28,49 @@ describe("Koulutustoimijan näkymä", () => {
     });
 
     it("Paikallisen tutkinnon osan luominen", () => {
-
         element(by.css("button[ng-click=\"add()\"]")).click();
-
         element.all(by.buttonText("Lisää")).get(1).click();
-
         element(by.css("input[ng-model=\"obj.tekstiKappale.nimi\"]")).sendKeys("tosa");
-
         element(by.css("button[ng-click=\"ok(obj)\"]")).click();
-
     });
 
     it("Tutkinnon osan toteutuksen kirjoittaminen", () => {
-
         element(by.css("button[ng-click=\"edit()\"]")).click();
-
         element(by.css("div[ng-model=\"osa.tekstiKappale.teksti\"]")).sendKeys("tosan sisältö");
-
         element(by.css("input[ng-model=\"kommentti\"]")).sendKeys("lisätty sisältö tosaan");
-
         element(by.css("button[ng-click=\"save()\"]")).click();
-
     });
 
     it("Poistaminen ja palauttaminen", () => {
-
         element(by.css("button[ng-click=\"edit()\"]")).click();
-
         element(by.css("button[ng-click=\"remove()\"]")).click();
-
         element(by.css("button[ng-click=\"ok()\"]")).click();
-
         element(by.css("button[ng-click=\"palauta(poistettu)\"]")).click();
-
     });
 
     it("Käsitteen lisääminen ja poistaminen", () => {
-
         element(by.css("a[ui-sref=\".kasitteet\"]")).click();
-
         element(by.css("button[ng-click=\"createKasite()\"]")).click();
-
         element(by.css("input[ng-model=\"newKasite.termi\"]")).sendKeys("käsite 1");
-
         element(by.css("div[ng-model=\"newKasite.selitys\"]")).sendKeys("Käsitteen 1 selitys");
-
         element(by.css("button[ng-click=\"postKasite(newKasite)\"]")).click();
-
         element(by.css("button[ng-click=\"remove(kasite)\"]")).click();
-
         element(by.css("button[ng-click=\"ok()\"]")).click();
-
     });
 
     it("Dokumentin generointi", () => {
-
         element(by.css("a[ui-sref=\".dokumentti\"]")).click();
-
         element(by.css("button[ng-click=\"generoi()\"]")).click();
-
         expect(element(by.linkText("Lataa PDF")).getTagName()).toBe("a");
-
     });
 
     it("Versiohistoriasta palauttamien", () => {
-
         element(by.css("a[ui-sref=\".sisalto\"]")).click();
-
         element(by.css("button[ng-click=\"add()\"]")).click();
 
         element.all(by.buttonText("Lisää")).get(0).click();
-
         element(by.css("input[ng-model=\"obj.tekstiKappale.nimi\"]")).sendKeys("tekstikappale");
-
         element(by.css("button[ng-click=\"ok(obj)\"]")).click();
-
         element(by.css("button[ng-click=\"edit()\"]")).click();
 
         const tkTeksti = element(by.css("div[ng-model=\"osa.tekstiKappale.teksti\"]"));
@@ -111,18 +79,14 @@ describe("Koulutustoimijan näkymä", () => {
         tkTeksti.sendKeys("tekstikappaleen vanha sisältö");
 
         element(by.css("button[ng-click=\"save()\"]")).click();
-
         element(by.css("button[ng-click=\"edit()\"]")).click();
 
         tkTeksti.clear();
         tkTeksti.sendKeys("tekstikappaleen uusi sisältö");
 
         element(by.css("button[ng-click=\"save()\"]")).click();
-
         element(by.css("a[ng-click=\"listRevisions()\"]")).click();
-
         element(by.linkText("2")).click();
-
         element(by.css("button[ng-click=\"restoreRevision()\"]")).click();
 
     });
