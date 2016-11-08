468d39139ffec73299c032b1ffa518048e451c33
Niko Junkala <niko.junkala@gofore.com>
2016-11-08 11:59:19 +0200

    Korjaus uusien opsien luontiin

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/peruste/TutkinnonOsaKaikkiDto.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/peruste/TutkinnonOsaKaikkiDto.java
@@ -16,6 +16,7 @@
 
 package fi.vm.sade.eperusteet.amosaa.dto.peruste;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import fi.vm.sade.eperusteet.amosaa.dto.teksti.LokalisoituTekstiDto;
 import java.util.List;
@@ -28,6 +29,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TutkinnonOsaKaikkiDto extends PerusteenOsaDto {
     private final String osanTyyppi = "tutkinnonosa";
     private LokalisoituTekstiDto tavoitteet;
