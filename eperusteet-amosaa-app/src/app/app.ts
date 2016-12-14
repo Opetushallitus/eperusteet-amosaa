abd369eb22c0f1089d606369864229ae30eb7460
Niko Junkala <niko.junkala@gofore.com>
2016-12-14 16:06:20 +0200

    Uusi yritys

--- eperusteet-amosaa-app/src/app/app.ts
+++ eperusteet-amosaa-app/src/app/app.ts
@@ -34,8 +34,8 @@ angular.module("app", [
 // HTTP configs
 .config(($httpProvider) => {
     $httpProvider.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-    $httpProvider.defaults.xsrfHeaderName = "X-XSRF-TOKEN-EPERUSTEET-AMOSAA";
-    $httpProvider.defaults.xsrfCookieName = "XSRF-TOKEN-EPERUSTEET-AMOSAA";
+    $httpProvider.defaults.xsrfHeaderName = "CSRF";
+    $httpProvider.defaults.xsrfCookieName = "CSRF";
 
     $httpProvider.interceptors.push(["$q", ($q) => ({
         response: (res) => res,
