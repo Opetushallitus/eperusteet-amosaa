922807ed50e9a98c09ba6d7362186739322ea6c7
Sauli Anto <sauli.anto@gofore.com>
2017-05-17 11:54:37 +0300

    EP-1023 Käänösten käsittelyä parannettu

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/dokumentti/impl/LocalizedMessagesServiceImpl.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/dokumentti/impl/LocalizedMessagesServiceImpl.java
@@ -55,16 +55,16 @@ public class LocalizedMessagesServiceImpl implements LocalizedMessagesService {
 
     @Override
     public String translate(String key, Kieli kieli) {
-        try {
-            // koitetaan ensin lokalisointipalvelusta
-            LokalisointiDto valueDto = lokalisointiService.get(key, kieli.toString());
-            if (valueDto != null) {
-                return valueDto.getValue();
-            }
+        // koitetaan ensin lokalisointipalvelusta
+        LokalisointiDto valueDto = lokalisointiService.get(key, kieli.toString());
+        if (valueDto != null) {
+            return valueDto.getValue();
+        }
 
+        try {
             return messageSource.getMessage(key, null, Locale.forLanguageTag(kieli.toString()));
         } catch (NoSuchMessageException ex) {
-            return key;
+            return "[" + kieli.toString() + " " + key + "]";
         }
     }
 }
