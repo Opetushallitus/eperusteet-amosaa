caba7811bd0dc7df71bda705a8e4e81497bd2f75
Niko Junkala <niko.junkala@gofore.com>
2017-09-15 11:28:00 +0300

    Julkaistut tutkinnonosat ja suorituspolut julkisiksi

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/ops/SisaltoViiteService.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/ops/SisaltoViiteService.java
@@ -21,10 +21,9 @@ import fi.vm.sade.eperusteet.amosaa.dto.RevisionDto;
 import fi.vm.sade.eperusteet.amosaa.dto.teksti.SisaltoViiteDto;
 import fi.vm.sade.eperusteet.amosaa.resource.locks.contexts.SisaltoViiteCtx;
 import fi.vm.sade.eperusteet.amosaa.service.locking.LockService;
+import java.util.List;
 import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PreAuthorize;
-
-import java.util.List;
 
 /**
  * @author mikkom
@@ -85,10 +84,10 @@ public interface SisaltoViiteService extends LockService<SisaltoViiteCtx> {
     @PreAuthorize("hasPermission({#ktId, #opsId}, 'opetussuunnitelma', 'MUOKKAUS')")
     void copySisaltoViiteet(@P("ktId") Long ktId, @P("opsId") Long opsId, List<Long> viitteet);
 
-    @PreAuthorize("hasPermission({#ktId, #opsId}, 'opetussuunnitelma', 'LUKU')")
+    @PreAuthorize("hasPermission({#ktId, #opsId}, 'opetussuunnitelma', 'ESITYS')")
     <T> List<T> getTutkinnonOsat(@P("ktId") Long ktId, @P("opsId") Long opsId, Class<T> aClass);
 
-    @PreAuthorize("hasPermission({#ktId, #opsId}, 'opetussuunnitelma', 'LUKU')")
+    @PreAuthorize("hasPermission({#ktId, #opsId}, 'opetussuunnitelma', 'ESITYS')")
     <T> List<T> getSuorituspolut(@P("ktId") Long ktId, @P("opsId") Long opsId, Class<T> aClass);
 }
 
