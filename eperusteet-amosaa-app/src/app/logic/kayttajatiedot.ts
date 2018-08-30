00725ab8e76d78a0b16a0c3287e7b92a26c65f56
nkala <niko.junkala@gofore.com>
2018-08-30 13:36:27 +0300

    Lisätty chat (#76)
    * Lisätty chat
    
    * Korjaus käyttäjätietojen esittämiseen niiden puuttuessa
    
    * Lisätään esikysely takaisin

--- eperusteet-amosaa-app/src/app/logic/kayttajatiedot.ts
+++ eperusteet-amosaa-app/src/app/logic/kayttajatiedot.ts
@@ -1,6 +1,12 @@
 namespace Kayttajatiedot {
-    export const parsiEsitysnimi = kayttajatieto =>
-        kayttajatieto.sukunimi
-            ? kayttajatieto.kutsumanimi + " " + kayttajatieto.sukunimi
-            : KaannaService.kaanna("muokkaajaa-ei-loytynyt") + " (" + kayttajatieto.oidHenkilo + ")";
+    export const parsiEsitysnimi = (kayttajatieto) => {
+        if (kayttajatieto) {
+            return (kayttajatieto.sukunimi && kayttajatieto.kutsumanimi)
+                ? kayttajatieto.kutsumanimi + " " + kayttajatieto.sukunimi
+                : KaannaService.kaanna("muokkaajaa-ei-loytynyt") + " (" + kayttajatieto.oidHenkilo + ")";
+        }
+        else {
+            return KaannaService.kaanna("tuntematon-kayttaja");
+        }
+    }
 }
