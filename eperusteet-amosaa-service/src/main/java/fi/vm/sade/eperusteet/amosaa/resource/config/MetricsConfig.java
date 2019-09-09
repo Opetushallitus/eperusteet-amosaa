4858d1e02d00c997badda0aa83605f19845b9fee
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2019-09-09 15:09:16 +0300

    EP-1779 #2

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/resource/config/MetricsConfig.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/resource/config/MetricsConfig.java
@@ -40,7 +40,7 @@ public class MetricsConfig extends MetricsConfigurerAdapter {
 
         registerReporter(ConsoleReporter
                 .forRegistry(metricRegistry)
-                .build()).start(1, TimeUnit.MINUTES);
+                .build()).start(1, TimeUnit.DAYS);
     }
 
 }
