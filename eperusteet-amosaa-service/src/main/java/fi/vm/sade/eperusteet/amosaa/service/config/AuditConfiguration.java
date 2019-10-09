d3ef861446fcdd5b25af0e4af1cf25d4572a20d2
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2019-10-09 13:37:44 +0300

    EP-1800

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/config/AuditConfiguration.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/config/AuditConfiguration.java
@@ -1,7 +1,8 @@
-package fi.vm.sade.eperusteet.amosaa.service.audit;
+package fi.vm.sade.eperusteet.amosaa.service.config;
 
 import fi.vm.sade.auditlog.ApplicationType;
 import fi.vm.sade.auditlog.Audit;
+import fi.vm.sade.eperusteet.amosaa.service.audit.LoggerForAudit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
