ff1f0705b62fe359888f76bde069f44f80374681
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-06-12 14:42:03 +0300

    EP-2216: lukotus

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/resource/locks/contexts/SisaltoViiteCtx.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/resource/locks/contexts/SisaltoViiteCtx.java
@@ -35,12 +35,21 @@ public class SisaltoViiteCtx extends OpsCtx {
         this.svId = svId;
     }
 
+    public SisaltoViiteCtx(Long ktId, Long opsId, Long svId) {
+        super(ktId, opsId);
+        this.svId = svId;
+    }
+
     public void setOsanId(Long svId) {
         this.svId = svId;
     }
 
     public static SisaltoViiteCtx of(Long svId) {
         return new SisaltoViiteCtx(svId);
+    }
+
+    public static SisaltoViiteCtx of(Long ktId, Long opsId, Long svId) {
+        return new SisaltoViiteCtx(ktId, opsId, svId);
     }
 
 }
