033944686c0a1a86555b66ace15d042ceb13ceda
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-09-06 10:40:54 +0300

    EP-2692

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/domain/validation/ValidHtmlValidatorBase.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/domain/validation/ValidHtmlValidatorBase.java
@@ -53,7 +53,9 @@ public abstract class ValidHtmlValidatorBase {
     private boolean isValidUrls(String teksti) {
         Document doc = Jsoup.parse(teksti);
         Elements links = doc.select("a[href]");
-        return links.stream().allMatch(link -> urlValidator.isValid(link.attr("abs:href"))
-                || emailValidator.isValid(link.attr("href").replace("mailto:", "")));
+        return links.stream().allMatch(link ->
+                !link.attr("routenode").isEmpty()
+                        || urlValidator.isValid(link.attr("abs:href"))
+                        || emailValidator.isValid(link.attr("href").replace("mailto:", "")));
     }
 }
