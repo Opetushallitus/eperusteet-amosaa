181b6c733e6b2fc7e24ca4c68add6152a25e5302
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2022-02-08 10:53:15 +0200

    EP-2977: Importit järjestetty uusiksi

--- eperusteet-amosaa-service/src/test/java/fi/vm/sade/eperusteet/amosaa/service/SisaltoViiteServiceImplTest.java
+++ eperusteet-amosaa-service/src/test/java/fi/vm/sade/eperusteet/amosaa/service/SisaltoViiteServiceImplTest.java
@@ -1,22 +1,5 @@
 package fi.vm.sade.eperusteet.amosaa.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
-import org.assertj.core.api.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-
 import fi.vm.sade.eperusteet.amosaa.domain.koulutustoimija.Opetussuunnitelma;
 import fi.vm.sade.eperusteet.amosaa.dto.ops.SuorituspolkuRiviDto;
 import fi.vm.sade.eperusteet.amosaa.dto.peruste.RakenneModuuliDto;
@@ -26,13 +9,27 @@ import fi.vm.sade.eperusteet.amosaa.dto.teksti.SuorituspolkuDto;
 import fi.vm.sade.eperusteet.amosaa.service.ops.impl.SisaltoViiteServiceImpl;
 import fi.vm.sade.eperusteet.amosaa.service.peruste.PerusteCacheService;
 import fi.vm.sade.eperusteet.amosaa.test.TestClassCreatorUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SisaltoViiteServiceImplTest {
 
-    private List<UUID> rakennemoduulitunnisteet = Arrays.asList(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID(),
+    private final List<UUID> rakennemoduulitunnisteet = Arrays.asList(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID(),
             UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID());
-    private List<UUID> rakennemoduulitunnisteetPiilotetut = Arrays.asList(rakennemoduulitunnisteet.get(1),
+    private final List<UUID> rakennemoduulitunnisteetPiilotetut = Arrays.asList(rakennemoduulitunnisteet.get(1),
             rakennemoduulitunnisteet.get(3), rakennemoduulitunnisteet.get(5));
 
     @Mock
