123e3b1985e2618b4a841ed8ff8f1bc5db812f24
oplekal <53811784+oplekal@users.noreply.github.com>
2022-08-22 10:28:12 +0300

    EP-3274 (#574)
    * EP-3274
    
    * github actions generated spec files [skip actions]
    
    Co-authored-by: GitHub Actions runner <>

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/KoulutuksenOsaDto.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/KoulutuksenOsaDto.java
@@ -1,11 +1,13 @@
 package fi.vm.sade.eperusteet.amosaa.dto.teksti;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import fi.vm.sade.eperusteet.amosaa.dto.peruste.KoulutusOsanKoulutustyyppi;
 import fi.vm.sade.eperusteet.amosaa.dto.peruste.KoulutusOsanTyyppi;
 import java.util.List;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KoulutuksenOsaDto extends KoulutuksenOsaKevytDto {
     
     private Integer laajuusMinimi;
