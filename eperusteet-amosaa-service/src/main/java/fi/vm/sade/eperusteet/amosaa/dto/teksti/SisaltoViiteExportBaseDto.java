c019c19327bc268fef6159c0ffc0bd73bdaffb72
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2022-09-22 15:53:25 +0300

    EP-3314

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/SisaltoViiteExportBaseDto.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/SisaltoViiteExportBaseDto.java
@@ -1,6 +1,9 @@
 package fi.vm.sade.eperusteet.amosaa.dto.teksti;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import fi.vm.sade.eperusteet.amosaa.domain.SisaltoTyyppi;
+import fi.vm.sade.eperusteet.amosaa.dto.peruste.CachedPerusteKevytDto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +12,10 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SisaltoViiteExportBaseDto {
     private Long id;
     private SisaltoTyyppi tyyppi;
+    private CachedPerusteKevytDto peruste;
 }
