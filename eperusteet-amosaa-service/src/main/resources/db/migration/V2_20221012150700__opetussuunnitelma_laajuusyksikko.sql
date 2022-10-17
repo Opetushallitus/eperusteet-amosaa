3d35727930448514cd127b73848690573f23212e
Timo Paasila <timo.paasila@gofore.com>
2022-10-17 11:43:55 +0300

    EP-3365 - lisätty LaajuusYksikko opintokokonaisuudelle

--- eperusteet-amosaa-service/src/main/resources/db/migration/V2_20221012150700__opetussuunnitelma_laajuusyksikko.sql
+++ eperusteet-amosaa-service/src/main/resources/db/migration/V2_20221012150700__opetussuunnitelma_laajuusyksikko.sql
@@ -1,4 +1,4 @@
 ALTER TABLE opintokokonaisuus ADD COLUMN laajuusYksikko varchar(255);
 ALTER TABLE opintokokonaisuus_aud ADD COLUMN laajuusYksikko varchar(255);
-UPDATE opintokokonaisuus SET laajuusYksikko = 'OSAAMISPISTE';
-UPDATE opintokokonaisuus_aud SET laajuusYksikko = 'OSAAMISPISTE';
+UPDATE opintokokonaisuus SET laajuusYksikko = 'OPINTOPISTE';
+UPDATE opintokokonaisuus_aud SET laajuusYksikko = 'OPINTOPISTE';
