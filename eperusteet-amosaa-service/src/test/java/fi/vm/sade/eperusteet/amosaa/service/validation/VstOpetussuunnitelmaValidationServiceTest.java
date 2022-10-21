59ef0e40ea8e397d4dc175d8a43b39b34bc1c26b
Timo Paasila <timo.paasila@gofore.com>
2022-10-21 08:56:49 +0300

    EP-3366

--- eperusteet-amosaa-service/src/test/java/fi/vm/sade/eperusteet/amosaa/service/validation/VstOpetussuunnitelmaValidationServiceTest.java
+++ eperusteet-amosaa-service/src/test/java/fi/vm/sade/eperusteet/amosaa/service/validation/VstOpetussuunnitelmaValidationServiceTest.java
@@ -2,22 +2,23 @@ package fi.vm.sade.eperusteet.amosaa.service.validation;
 
 import fi.vm.sade.eperusteet.amosaa.domain.SisaltoTyyppi;
 import fi.vm.sade.eperusteet.amosaa.dto.koulutustoimija.OpetussuunnitelmaBaseDto;
+import fi.vm.sade.eperusteet.amosaa.dto.peruste.LaajuusYksikko;
 import fi.vm.sade.eperusteet.amosaa.dto.teksti.LokalisoituTekstiDto;
 import fi.vm.sade.eperusteet.amosaa.dto.teksti.OpintokokonaisuusDto;
 import fi.vm.sade.eperusteet.amosaa.dto.teksti.SisaltoViiteDto;
 import fi.vm.sade.eperusteet.amosaa.dto.teksti.TekstiKappaleDto;
-import fi.vm.sade.eperusteet.amosaa.repository.teksti.SisaltoviiteRepository;
 import fi.vm.sade.eperusteet.amosaa.service.ops.OpetussuunnitelmaValidationService;
 import fi.vm.sade.eperusteet.amosaa.service.ops.SisaltoViiteService;
 import fi.vm.sade.eperusteet.amosaa.service.ops.impl.VstOpetussuunnitelmaValidationService;
 import fi.vm.sade.eperusteet.amosaa.service.util.Validointi;
 import fi.vm.sade.eperusteet.amosaa.test.AbstractIntegrationTest;
-import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
@@ -32,23 +33,21 @@ public class VstOpetussuunnitelmaValidationServiceTest extends AbstractIntegrati
     @Autowired
     private SisaltoViiteService sisaltoViiteService;
 
+    OpetussuunnitelmaBaseDto vstOps;
+
+    SisaltoViiteDto.Matala root;
+
+    @Before
+    public void init() {
+        useProfileVst();
+        vstOps = createOpetussuunnitelma(ops -> ops.setPerusteId(35820L));
+        root = sisaltoViiteService.getSisaltoRoot(getKoulutustoimijaId(), vstOps.getId());
+    }
+
     @Test
     public void testSisallostaPuuttuuOpintokokonaisuusNimi() {
-        useProfileVst();
-        OpetussuunnitelmaBaseDto vstOps = createOpetussuunnitelma(ops -> ops.setPerusteId(35820L));
-        SisaltoViiteDto.Matala root = sisaltoViiteService.getSisaltoRoot(getKoulutustoimijaId(), vstOps.getId());
-        SisaltoViiteDto.Matala opintokokonaisuus1 = sisaltoViiteService.addSisaltoViite(getKoulutustoimijaId(), vstOps.getId(), root.getId(),
-                createSisalto(sisaltoViiteDto -> {
-                    sisaltoViiteDto.setTyyppi(SisaltoTyyppi.OPINTOKOKONAISUUS);
-                    sisaltoViiteDto.setTekstiKappale(new TekstiKappaleDto());
-                    sisaltoViiteDto.setOpintokokonaisuus(new OpintokokonaisuusDto());
-                }));
-        SisaltoViiteDto.Matala opintokokonaisuus2 = sisaltoViiteService.addSisaltoViite(getKoulutustoimijaId(), vstOps.getId(), root.getId(),
-                createSisalto(sisaltoViiteDto -> {
-                    sisaltoViiteDto.setTyyppi(SisaltoTyyppi.OPINTOKOKONAISUUS);
-                    sisaltoViiteDto.setTekstiKappale(new TekstiKappaleDto());
-                    sisaltoViiteDto.setOpintokokonaisuus(new OpintokokonaisuusDto());
-                }));
+        SisaltoViiteDto.Matala opintokokonaisuus1 = createOpintokokonaisuus();
+        SisaltoViiteDto.Matala opintokokonaisuus2 = createOpintokokonaisuus();
 
         Validointi validointi = vstOpetussuunnitelmaValidationService.validoi(getKoulutustoimijaId(), vstOps.getId());
         assertThat(validointi.getVirheet().stream()
@@ -64,6 +63,39 @@ public class VstOpetussuunnitelmaValidationServiceTest extends AbstractIntegrati
         sisaltoViiteService.updateSisaltoViite(getKoulutustoimijaId(), vstOps.getId(), opintokokonaisuus2.getId(), opintokokonaisuus2);
         validointi = vstOpetussuunnitelmaValidationService.validoi(getKoulutustoimijaId(), vstOps.getId());
         assertThat(validointi.getVirheet().stream().noneMatch(virhe -> virhe.getSyy().equals(VstOpetussuunnitelmaValidationService.SISALLOSSA_NIMETTOMIA_OPINTOKOKONAISUUKSIA))).isTrue();
+    }
 
+    @Test
+    public void testSisallostaPuuttuuOpintokokonaisuusLaajuusJaYksikko() {
+        // validoidaan vstOps:n defaultina luotu opintokokonaisuus
+        Validointi validointi = vstOpetussuunnitelmaValidationService.validoi(getKoulutustoimijaId(), vstOps.getId());
+        assertThat(validointi.getVirheet()).extracting(Validointi.Virhe::getSyy)
+                .contains("sisallossa-opintokokonaisuuksia-ilman-laajuutta")
+                .contains("sisallossa-opintokokonaisuuksia-ilman-laajuusyksikkoa");
+
+        SisaltoViiteDto viite = sisaltoViiteService.getSisaltoviitteet(getKoulutustoimijaId(), vstOps.getId(), SisaltoTyyppi.OPINTOKOKONAISUUS).get(0);
+
+        viite.getOpintokokonaisuus().setLaajuus(BigDecimal.valueOf(30.0));
+        sisaltoViiteService.updateSisaltoViite(getKoulutustoimijaId(), vstOps.getId(), viite.getId(), viite);
+        validointi = vstOpetussuunnitelmaValidationService.validoi(getKoulutustoimijaId(), vstOps.getId());
+        assertThat(validointi.getVirheet()).extracting(Validointi.Virhe::getSyy)
+                .doesNotContain("sisallossa-opintokokonaisuuksia-ilman-laajuutta")
+                .contains("sisallossa-opintokokonaisuuksia-ilman-laajuusyksikkoa");
+
+        viite.getOpintokokonaisuus().setLaajuusYksikko(LaajuusYksikko.OPINTOPISTE);
+        sisaltoViiteService.updateSisaltoViite(getKoulutustoimijaId(), vstOps.getId(), viite.getId(), viite);
+        validointi = vstOpetussuunnitelmaValidationService.validoi(getKoulutustoimijaId(), vstOps.getId());
+        assertThat(validointi.getVirheet()).extracting(Validointi.Virhe::getSyy)
+                .doesNotContain("sisallossa-opintokokonaisuuksia-ilman-laajuutta")
+                .doesNotContain("sisallossa-opintokokonaisuuksia-ilman-laajuusyksikkoa");
+    }
+
+    private SisaltoViiteDto.Matala createOpintokokonaisuus() {
+        return sisaltoViiteService.addSisaltoViite(getKoulutustoimijaId(), vstOps.getId(), root.getId(),
+                createSisalto(sisaltoViiteDto -> {
+                    sisaltoViiteDto.setTyyppi(SisaltoTyyppi.OPINTOKOKONAISUUS);
+                    sisaltoViiteDto.setTekstiKappale(new TekstiKappaleDto());
+                    sisaltoViiteDto.setOpintokokonaisuus(new OpintokokonaisuusDto());
+                }));
     }
 }
