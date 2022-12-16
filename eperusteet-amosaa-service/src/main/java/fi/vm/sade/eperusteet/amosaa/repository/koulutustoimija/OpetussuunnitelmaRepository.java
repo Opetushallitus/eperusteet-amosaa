4e23aeb31172cc797a7f7c2ba29b18678d0c8647
timopaasila <113356513+timopaasila@users.noreply.github.com>
2022-12-16 12:44:22 +0200

    EP-3465 (#650)
    * EP-3465
    
    * EP-3465

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/repository/koulutustoimija/OpetussuunnitelmaRepository.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/repository/koulutustoimija/OpetussuunnitelmaRepository.java
@@ -133,8 +133,8 @@ public interface OpetussuunnitelmaRepository extends JpaWithVersioningRepository
             "AND nimi.kieli = :kieli " +
             "AND (COALESCE(:nimi, '') = '' OR LOWER(nimi.teksti) LIKE LOWER(CONCAT('%', :nimi,'%'))) " +
             "AND (:jotpa = false OR o.jotpatyyppi IS NOT NULL) " +
-            "AND ((:poistettu = false AND tila != 'POISTETTU' AND ((:julkaistu = false AND o.julkaisut IS EMPTY) OR (:julkaistu = true AND o.julkaisut IS NOT EMPTY))) " +
-            "    OR (:poistettu = true AND tila = 'POISTETTU')) "+
+            "AND ((:poistettu = false AND tila != 'POISTETTU' AND ((:julkaistuTaiValmis = false AND (o.julkaisut IS EMPTY AND o.tila = 'LUONNOS')) OR (:julkaistuTaiValmis = true AND (o.julkaisut IS NOT EMPTY OR o.tila = 'VALMIS')))) " +
+            "    OR (:poistettu = true AND tila = 'POISTETTU')) " +
             "AND o.tyyppi = :tyyppi ")
     Page<Opetussuunnitelma> findByKoulutustoimijaInAndPerusteKoulutustyyppiInAndOpsTyyppi(
             @Param("koulutustoimijat") List<Long> ktId,
@@ -142,7 +142,7 @@ public interface OpetussuunnitelmaRepository extends JpaWithVersioningRepository
             @Param("nimi") String nimi,
             @Param("kieli") Kieli kieli,
             @Param("jotpa") Boolean jotpa,
-            @Param("julkaistu") Boolean julkaistu,
+            @Param("julkaistuTaiValmis") Boolean julkaistuTaiValmis,
             @Param("tyyppi") OpsTyyppi tyyppi,
             @Param("poistettu") Boolean poistettu,
             Pageable pageable);
