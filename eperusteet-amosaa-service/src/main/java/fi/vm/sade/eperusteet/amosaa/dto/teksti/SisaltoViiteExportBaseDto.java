d3b08d67e7e5e92f508a0937344744afee82f08b
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-03-29 13:48:08 +0300

    EP-3654

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/SisaltoViiteExportBaseDto.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/SisaltoViiteExportBaseDto.java
@@ -1,5 +1,6 @@
 package fi.vm.sade.eperusteet.amosaa.dto.teksti;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import fi.vm.sade.eperusteet.amosaa.domain.SisaltoTyyppi;
@@ -18,4 +19,18 @@ public class SisaltoViiteExportBaseDto {
     private Long id;
     private SisaltoTyyppi tyyppi;
     private CachedPerusteKevytDto peruste;
+
+    private Long linkattuOps;
+    private Long linkattuSisaltoViiteId;
+    private Long linkattuPerusteId;
+    @JsonIgnore
+    private SisaltoTyyppi linkattuTyyppi;
+
+    public SisaltoTyyppi getTyyppi() {
+        if (linkattuTyyppi != null) {
+            return linkattuTyyppi;
+        }
+
+        return tyyppi;
+    }
 }
--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/SisaltoViiteExportDto.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/SisaltoViiteExportDto.java
@@ -1,7 +1,9 @@
 package fi.vm.sade.eperusteet.amosaa.dto.teksti;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import fi.vm.sade.eperusteet.amosaa.domain.SisaltoTyyppi;
 import fi.vm.sade.eperusteet.amosaa.dto.peruste.PerusteenOsaDto;
 import lombok.*;
 
