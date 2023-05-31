9193b69bf8dbf44a7bc7c33c2f25697dcfeaeeca
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-05-31 11:28:28 +0300

    bugfix/EP-3738

--- eperusteet-amosaa-service/src/main/resources/db/migration/V2_20230524120000__valmistilaiset_update.sql
+++ eperusteet-amosaa-service/src/main/resources/db/migration/V2_20230524120000__valmistilaiset_update.sql
@@ -1,1 +1,3 @@
+ALTER TABLE opetussuunnitelma DISABLE TRIGGER tg_refresh_opetussuunnitelma_data_view_after_tila_update;
 UPDATE opetussuunnitelma SET tila = 'LUONNOS' WHERE tila = 'VALMIS' AND tyyppi = 'YHTEINEN';
+ALTER TABLE opetussuunnitelma ENABLE TRIGGER tg_refresh_opetussuunnitelma_data_view_after_tila_update;
