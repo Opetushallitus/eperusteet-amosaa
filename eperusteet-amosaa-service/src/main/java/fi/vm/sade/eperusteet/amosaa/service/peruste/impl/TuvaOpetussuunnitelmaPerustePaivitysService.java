4ec7d599c89c87e84a899ee983b3fa5d50b9228d
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-08-04 13:12:48 +0300

    EP-3830

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/peruste/impl/TuvaOpetussuunnitelmaPerustePaivitysService.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/service/peruste/impl/TuvaOpetussuunnitelmaPerustePaivitysService.java
@@ -8,18 +8,16 @@ import fi.vm.sade.eperusteet.amosaa.domain.koulutustoimija.OpsTyyppi;
 import fi.vm.sade.eperusteet.amosaa.domain.teksti.SisaltoViite;
 import fi.vm.sade.eperusteet.amosaa.domain.tutkinnonosa.KoulutuksenosanPaikallinenTarkennus;
 import fi.vm.sade.eperusteet.amosaa.dto.peruste.PerusteKaikkiDto;
-import fi.vm.sade.eperusteet.amosaa.dto.peruste.PerusteenOsaViiteDto;
 import fi.vm.sade.eperusteet.amosaa.repository.koulutustoimija.OpetussuunnitelmaRepository;
 import fi.vm.sade.eperusteet.amosaa.repository.teksti.SisaltoviiteRepository;
 import fi.vm.sade.eperusteet.amosaa.service.exception.BusinessRuleViolationException;
-import fi.vm.sade.eperusteet.amosaa.service.external.EperusteetService;
 import fi.vm.sade.eperusteet.amosaa.service.ops.OpetussuunnitelmaSisaltoCreateService;
-import fi.vm.sade.eperusteet.amosaa.service.peruste.OpetussuunnitelmaPerustePaivitysService;
 import fi.vm.sade.eperusteet.amosaa.service.util.CollectionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -55,14 +53,17 @@ public class TuvaOpetussuunnitelmaPerustePaivitysService extends MuutOpetussuunn
                 .findFirst()
                 .orElseThrow(() -> new BusinessRuleViolationException("toteutussuunnitelmalta-puutteellinen"));
 
-        koulutuksenosatViite.setLapset(CollectionUtil.treeToStream(opetussuunnitelma.getSisaltoviitteet(), SisaltoViite::getLapset)
-                .filter(viite -> viite.getTyyppi().equals(SisaltoTyyppi.KOULUTUKSENOSA))
-                .peek(koulutuksenosaViite -> {
-                    koulutuksenosaViite.setVanhempi(koulutuksenosatViite);
-                    if (koulutuksenosaViite.getKoulutuksenosa().getPaikallinenTarkennus() == null) {
-                        koulutuksenosaViite.getKoulutuksenosa().setPaikallinenTarkennus(new KoulutuksenosanPaikallinenTarkennus());
-                    }
-                }).collect(Collectors.toList()));
-    }
+        koulutuksenosatViite.setLapset(new ArrayList<>(
+                CollectionUtil.treeToStream(opetussuunnitelma.getSisaltoviitteet(), SisaltoViite::getLapset)
+                    .filter(viite -> viite.getTyyppi().equals(SisaltoTyyppi.KOULUTUKSENOSA))
+                    .peek(koulutuksenosaViite -> {
+                        koulutuksenosaViite.getVanhempi().getLapset().remove(koulutuksenosaViite);
+                        koulutuksenosaViite.setVanhempi(koulutuksenosatViite);
+                        if (koulutuksenosaViite.getKoulutuksenosa().getPaikallinenTarkennus() == null) {
+                            koulutuksenosaViite.getKoulutuksenosa().setPaikallinenTarkennus(new KoulutuksenosanPaikallinenTarkennus());
+                        }
+                    }).collect(Collectors.toList())));
 
+        sisaltoviiteRepository.save(koulutuksenosatViite);
+    }
 }
