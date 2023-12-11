6133e3879962c3384a25291a1db7f0e7c989d4dc
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-12-11 11:18:26 +0200

    EP-4007

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/ArvioinninKohdeExportDto.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/dto/teksti/ArvioinninKohdeExportDto.java
@@ -1,6 +1,8 @@
 package fi.vm.sade.eperusteet.amosaa.dto.teksti;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import fi.vm.sade.eperusteet.amosaa.dto.Reference;
 import fi.vm.sade.eperusteet.amosaa.dto.peruste.ArviointiasteikkoDto;
 import fi.vm.sade.eperusteet.amosaa.dto.peruste.OsaamistasonKriteeriDto;
 import lombok.AllArgsConstructor;
@@ -18,4 +20,7 @@ public class ArvioinninKohdeExportDto {
     private LokalisoituTekstiDto selite;
     private ArviointiasteikkoDto arviointiasteikko;
     private Set<OsaamistasonKriteeriDto> osaamistasonKriteerit;
+
+    @JsonProperty("_arviointiasteikko")
+    private Reference arviointiasteikkoRef;
 }
