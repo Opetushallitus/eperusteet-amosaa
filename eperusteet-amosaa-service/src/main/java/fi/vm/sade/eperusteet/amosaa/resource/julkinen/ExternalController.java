8a1f286ee275fe48967b38b022578e87dc04527f
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-02-27 10:31:38 +0200

    cas client session

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/resource/julkinen/ExternalController.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/resource/julkinen/ExternalController.java
@@ -73,7 +73,7 @@ public class ExternalController {
     
     @RequestMapping(value = "/opetussuunnitelma/{koulutustoimijaId:\\d+}/{opsId:\\d+}", method = RequestMethod.GET)
     @ApiOperation(value = "Opetussuunnitelman tietojen haku koulutustoimijan ja opetussuunnitelman id:llä")
-    public OpetussuunnitelmaKaikkiDto getPublicOpetussuunnitelma(
+    public OpetussuunnitelmaKaikkiDto getPublicOpetussuunnitelmaKoulutustoimija(
             @PathVariable final Long koulutustoimijaId,
             @PathVariable final Long opsId
     ) {
