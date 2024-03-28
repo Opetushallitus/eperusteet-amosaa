573a72036c63aad135f40e23698ac34c33ee80e4
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-03-28 14:07:28 +0200

    julkinen pdf fix

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/resource/julkinen/JulkinenController.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/resource/julkinen/JulkinenController.java
@@ -32,6 +32,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -222,7 +223,7 @@ public class JulkinenController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "ktId", dataType = "string", paramType = "path")
     })
-    @RequestMapping(value = "/koulutustoimijat/{ktId}/opetussuunnitelmat/{opsId}/dokumentti", method = RequestMethod.GET)
+    @RequestMapping(value = "/koulutustoimijat/{ktId}/opetussuunnitelmat/{opsId}/dokumentti", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<Object> getDokumentti(@ApiIgnore @ModelAttribute("ktId") final Long ktId,
                                                 @PathVariable final Long opsId,
                                                 @RequestParam(defaultValue = "fi") final String kieli,
