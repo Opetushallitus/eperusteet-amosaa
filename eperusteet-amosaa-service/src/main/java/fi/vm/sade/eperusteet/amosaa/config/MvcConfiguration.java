f528d65f8a040f0273aa3f6edbde834b69fe6740
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-04-23 15:27:59 +0300

    EP-4199

--- eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/config/MvcConfiguration.java
+++ eperusteet-amosaa-service/src/main/java/fi/vm/sade/eperusteet/amosaa/config/MvcConfiguration.java
@@ -42,6 +42,7 @@ public class MvcConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/ui/").setViewName("forward:/ui/index.html");
         registry.addRedirectViewController("/ui", "/ui/");
+        registry.addRedirectViewController("/swagger", "/swagger/index.html");
     }
 
     @Override
